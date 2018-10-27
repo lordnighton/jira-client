12f27420b05ac1a90216ebc3e90189484e6e6924
Lord Nighton <lordnightonnik@gmail.com>
2018-10-27 02:23:36 -0300

    Providing more configuration -- endpoints could be read from properties file from now

--- src/main/java/com/ubs/qa/jira/client/ClientApplication.java
+++ src/main/java/com/ubs/qa/jira/client/ClientApplication.java
@@ -5,6 +5,8 @@ import okhttp3.Request;
 import okhttp3.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,23 +16,26 @@ public class ClientApplication implements CommandLineRunner {
 
 	private static Logger LOG = LoggerFactory.getLogger(ClientApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClientApplication.class, args);
-	}
+    @Value("${jira.session.endpoint}")
+    private String jiraSessionAPI;
+
+    @Autowired
+    private OkHttpClient okHttpClient;
 
 	@Override
 	public void run(String... strings) throws Exception {
         LOG.info("EXECUTING : command line runner");
-        OkHttpClient client = new OkHttpClient();
-
-        String url = "https://jsonplaceholder.typicode.com/posts";
 
         Request request = new Request.Builder()
-                .url(url)
+                .url(jiraSessionAPI)
                 .build();
 
-        Response response = client.newCall(request).execute();
+        Response response = okHttpClient.newCall(request).execute();
         LOG.info(response.body().string());
 	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
 
 }
